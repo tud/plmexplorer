97abd0306ee106cf85e3f82c0bdac8b5aa14a37a
Stefano Manieri <stefano@manieri.info>
2008-08-06 15:24:22 +0200

    corretto bug visualizzazione IE

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,7 @@
 jQuery(document).ready(function(){
 	jQuery('#topsearch').corner("20px top");
 	jQuery('#bottomsearch').corner("20px bottom");
-	jQuery('#header').corner("20px top");
-	jQuery('#menu').corner("20px bottom");
+	jQuery('#header').corner("20px");
 	jQuery('#content').corner("20px");
 	jQuery('#footer').corner("10px");
 });
