93882385e07066d9c29bceb9998dcc0475654aea
Sergio Gallelli <sergio.gallelli@gmail.com>
2008-08-08 15:37:44 +0200

    Prima implementazione rudimentale Find

--- app/controllers/brecords_controller.rb
+++ app/controllers/brecords_controller.rb
@@ -1,11 +1,79 @@
 class BrecordsController < ApplicationController
   
+  ESCAPE = '\\'
+  
   def index
-    @rectype = ""
+    @rectype = ''
   end
 
   def show
-    @rectype = params[:rectype].upcase
+    if request.post?
+      brecord = params[:brecord]
+      @rectype = brecord[:rec_brectype].upcase
+      @clause = ''
+      name = '*'
+      cage_code = nil
+      brecord.each do |key, value|
+        downkey = key.downcase
+        case downkey
+        when /rec_(.+)/
+          field = $1
+          if field != 'bdesc'
+            normalize(value)
+          end
+          if field == 'name'
+            name = value
+          elsif field == 'cage_code'
+            cage_code = value
+          else
+            @clause = add_condition(@clause, field, value)
+          end
+        when /uda_(.+)/
+          field = $1
+        else
+          print "Illegal field name: #{key}"
+        end
+      end
+      unless matches_any(name) && matches_any(cage_code)
+        value = name
+        value += ('&' + cage_code) unless cage_code.nil?
+        @clause = add_condition(@clause, 'brecname', value)
+      end
+    else
+      @rectype = params[:rectype].upcase
+      @clause = "brectype = '" + @rectype + "'"
+    end
+  end
+
+  def normalize(string)
+    string ||= ''
+    string.sub!(/ +$/,'')
+    string.sub!(/^$/,'*')
+    string.upcase!
+    return string
+  end
+
+  def matches_any(string)
+    string.nil? || /^\**$/.match(string)
+  end
+
+  def add_condition(clause, field, value)
+    if !matches_any(value)
+      clause ||= ''
+      clause += ' AND ' unless clause.empty?
+      if value.index(/[*?]/).nil?
+        # there are no wildcards in value
+        clause += "#{field} = '#{value}'"
+      else
+        # translate wildcards into SQL
+        value.gsub!(/%/,ESCAPE+'%')
+        value.gsub!(/_/,ESCAPE+'_')
+        value.gsub!(/\*/,'%')
+        value.gsub!(/\?/,'_')
+        clause += "#{field} LIKE '#{value}' ESCAPE '#{ESCAPE}'"
+      end
+    end
+    return clause
   end
 
   def grid_records
@@ -13,7 +81,8 @@ class BrecordsController < ApplicationController
     limit = (params[:rows]).to_i
     sidx = params[:sidx]
     sord = params[:sord] || "desc"
-    rectype = params[:rectype] || "PART"
+    clause = params[:clause]
+    puts clause
 
     start = ((page-1) * limit).to_i
     #start = (limit*(page-limit)).to_i
@@ -49,9 +118,9 @@ class BrecordsController < ApplicationController
 
       query = searchField + " " + oper
       #puts '----> query=' + query
-      conditions = "brectype = '" + rectype+"' AND " + query
+      conditions = clause + " AND " + query
     else
-      conditions = "brectype = '" + rectype+"'"  
+      conditions = clause
     end
 
     @brecords = Brecord.find :all,
