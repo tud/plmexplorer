883debfe4739a5190a010d13bac4f6bd1308422c
Stefano Manieri <stefano@manieri.info>
2009-01-10 11:26:04 +0100

    tolto messaggio di firebug

--- public/javascripts/onfirstload.js
+++ public/javascripts/onfirstload.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-	console.log(">"+$('#cFind').html()+"<");
 	if ($('#cFind').html() == "") {
 		$('#cFind').load('/rectype/part/find');
 		$("#cMenuTabs > ul").tabs('select',0);
