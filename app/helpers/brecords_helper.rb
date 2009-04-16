75eeff72b4de7d26d7d2ccc5aed732757b05758c
Stefano Manieri <stefano@manieri.info>
2009-04-16 11:20:11 +0200

    - implementato link per report IMGEXP - gestione rule per permessi sul menu report

--- app/helpers/brecords_helper.rb
+++ app/helpers/brecords_helper.rb
@@ -1,5 +1,5 @@
 module BrecordsHelper
-  
+
   def filter_names rectype
     filters = FORMS["#{rectype.upcase}"]
     filternames = Array.new
@@ -29,7 +29,7 @@ module BrecordsHelper
       ""
     end
   end
-  
+
   def navigation_find_items
     items = Array.new
     NAVIGATION['FIND'].each do |entry|
@@ -37,23 +37,30 @@ module BrecordsHelper
     end
     items
   end
-  
+
   def navigation_report_items
     items = Array.new
     NAVIGATION['REPORT'].each do |entry|
-      items << "<a class='wNavigation_report tooltip' href='"+entry['type']+"' title='"+entry['title']+"'>"+fam_img_tag(entry['iconclass'])+entry['label']+"</a>"
+      if entry['rule']
+        rc = send entry['rule']
+      else
+        rc = true
+      end
+      if rc == true
+        items << "<a class='wNavigation_report tooltip' href='"+entry['type']+"' title='"+entry['title']+"'>"+fam_img_tag(entry['iconclass'])+entry['label']+"</a>"
+      end
     end
     items
   end
-  
+
   def fam_img_tag famimg
     "<img src='/images/fam/"+famimg+".gif'/>&nbsp;"
   end
-  
+
   def fam_img_tag_rectype rectype
     fam_img_tag NAVIGATION['FIND'].find {|hash| hash['type'] == rectype}['iconclass']
   end
-  
+
   def default_reftypes relations, rectype
     reftypes = [ ]
     relations[rectype.upcase] && relations[rectype.upcase].each do |relation|
@@ -61,9 +68,12 @@ module BrecordsHelper
     end
     reftypes.join(',')
   end
-  
+
   def get_report_title action
     NAVIGATION['REPORT'].find {|hash| hash['type'] == action}['title']
   end
-  
+
+  def image_exporter
+    false
+  end
 end
