0fca79f7645db276ef962e20eb659463624450c8
Sergio Gallelli <sergio.gallelli@gmail.com>
2010-02-17 02:30:41 +0100

    Primo scheletro metodo "create" in brecords_controller.rb

--- app/controllers/brecords_controller.rb
+++ app/controllers/brecords_controller.rb
@@ -2,6 +2,8 @@ class BrecordsController < ApplicationController
 
   before_filter :authorize
 
+  require 'tempfile'
+
   def index
     @joins = ''
     uda_ref = 'u0'
@@ -490,6 +492,38 @@ class BrecordsController < ApplicationController
   end
   
   def create
+    brecord = params[:brecord]
+    if cookies[:report_print_queue] != brecord[:report_print_queue]
+      cookies[:report_print_queue] = { :value => brecord[:report_print_queue], :expires => 1.year.from_now }
+    end
+    edm_report[:edm_print_queue] = brecord[:report_print_queue]
+    edm_report[:edm_output_file] = brecord[:report_output_file]
+    logfile = Tempfile.new(edm_report[:report])
+    if ENV['RAILS_ENV'] == 'development'
+      # In sviluppo creo lo script DMS di lancio del report
+      # in un file temporaneo
+      script = logfile
+    else
+      # In produzione uso il file temporaneo come file di log
+      # e creo una pipe verso una sessione DMS remota, cui sottometto
+      # lo script di lancio del report
+      logfile.close
+      script = IO.popen("rsh #{PREF['SHERPA_SERVER']} dms > #{logfile.path}", "w")
+    end
+    script.puts("set db #{PREF['SHERPA_DB']}")
+    script.puts("set user #{session[:user][:buser]}")
+    script.puts("set project PIM")
+    script.puts("update record EDM_REPORT\\#{edm_report[:report]};1")
+    edm_report.each do |key, value|
+      if key != :report
+        script.puts "  change attribute #{key} \"#{value}\""
+      end
+    end
+    script.puts("end update")
+    script.puts("modify record EDM_REPORT\\#{edm_report[:report]}")
+    script.puts("end modify")
+    script.puts("exit")
+    script.close 
     render :layout => false
   end
 
